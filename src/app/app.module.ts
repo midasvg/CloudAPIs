245284561aa419dd4b8b3fb0f15c9abd6de00f63
Unknown <midasvg@hotmail.com>
2018-05-11 12:12:44 +0200

    Components + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,32 @@ import { NgModule,  NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BooksComponent } from './books/books.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HousesComponent } from './houses/houses.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BooksComponent,
+    CharactersComponent,
+    HousesComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent}
+    ],{ useHash: true }),
+    HttpClientModule,
+
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
